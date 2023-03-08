ac2766d2f6f115d89ba0d92c6a0caf5bc7130cdc
Gregory Caloin <gregchapauli@gmail.com>
2023-03-08 09:56:03 +0100

    Added name property

--- src/salle/tp/project/User.java
+++ src/salle/tp/project/User.java
@@ -1,5 +1,15 @@
 package salle.tp.project;
 
 public class User {
+	
+	private String name;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
